dcdc34523730ff496892cec64c9d8a3485026406
Marcel <marcelkaemper862@gmail.com>
2019-04-02 21:46:06 +0200

    Now writing 0's instead of 1's

--- fieldGen/fieldGen.js
+++ fieldGen/fieldGen.js
@@ -5,7 +5,7 @@ let size = 100;
 let array = "static int game["+size*size+"] = {";
 
 for(let i = 1; i<=size*size; i++){
-		array += "1";
+		array += "0";
 		if(i % 100 == 0){
 			array += "\n";
 		}
