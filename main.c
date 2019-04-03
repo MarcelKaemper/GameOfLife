ba40acd0768d5a97d3235cae8e06960dd87fb4a4
Marcel <marcelkaemper862@gmail.com>
2019-04-03 14:46:59 +0200

    Added second array; Added getNeighbourStatus function

--- main.c
+++ main.c
@@ -1,11 +1,15 @@
 #include <GL/glut.h>
 #include <stdio.h>
 #include "array.c"
+#include "array2.c"
 #include <math.h>
 
 void render(void);
 void gameTick(void);
 int neighbourValid(int position, int active);
+int getNeighbourStatus(int position, int active);
+
+int repeat = 1;
 
 int main(int argc, char **argv){
 		glutInit(&argc, argv);
@@ -23,19 +27,25 @@ int main(int argc, char **argv){
 void render(void){
 		glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
-		float size = 0.015;
+		float size = 0.03;
 		int cols = 100;
 		int rows = 100;
-
 
 		for(int i = 0; i<cols; i++){
 				for(int j = 0; j<rows; j++){
 						glBegin(GL_POLYGON);
-
-						if(game[i*100+j] == 0){
-							glColor3f(1,0,0);
+						if(repeat % 2 != 0){
+							if(game[i*100+j] == 0){
+								glColor3f(1,0,0);
+							}else{
+								glColor3f(0,0,1);
+							}
 						}else{
-							glColor3f(0,0,1);
+							if(game2[i*100+j] == 0){
+								glColor3f(1,0,0);
+							}else{
+								glColor3f(0,0,1);
+							}
 						}
 						glVertex2f(-1+size*j,1-size*i);
 
@@ -49,7 +59,9 @@ void render(void){
 						glFlush();
 				}
 		}
+
 		gameTick();
+		repeat += 1;
 
 		glutPostRedisplay();
 		glutSwapBuffers();
@@ -70,38 +82,71 @@ void gameTick(void){
 							}
 						}
 						// If alive
-						if(game[activeCell] == 1){
-								// Rule 1 - Underpopulation
-								printf("%d", neighbours);
-								/* char c; */
-								/* scanf("%c", &c); */
+						//
+						if(repeat % 2 != 0){
+							if(game[activeCell] == 1){
+									// Rule 1 - Underpopulation
+									/* printf("%d", neighbours); */
+									/* char c; */
+									/* scanf("%c", &c); */
+									if(neighbours < 2){
+										game2[activeCell] = 0;
+									// Rule 2
+									}else if((neighbours == 2) || (neighbours == 3)){
+										game2[activeCell] = 1;
 
-								if(neighbours < 2){
-									game[activeCell] = 0;
-								// Rule 2
-								}else if((neighbours == 2) || (neighbours == 3)){
+									// Rule 3 - Overpopulation
+									}else if(neighbours > 3){
+										game2[activeCell] = 0;
+									}
+									
+									// Kill
+									/* game[i*100+j] = 0; */
 
-								// Rule 3 - Overpopulation
-								}else if(neighbours > 3){
-									game[activeCell] = 0;
-								}
-								
-								// Kill
-								/* game[i*100+j] = 0; */
-
-						//If dead
+							//If dead
+							}else{
+									// Rule 4 - Reproduction
+										if(neighbours == 3){
+											game2[activeCell] = 1;
+										}
+										
+										// Revive
+										/* game[i*100+j] = 1; */
+							}
 						}else{
-								// Rule 4 - Reproduction
-								if(neighbours == 3){
-									game[activeCell] = 1;
-								}
-								
-								// Revive
-								/* game[i*100+j] = 1; */
+								if(game2[activeCell] == 1){
+										// Rule 1 - Underpopulation
+										/* printf("%d", neighbours); */
+										/* char c; */
+										/* scanf("%c", &c); */
 
+										if(neighbours < 2){
+											game[activeCell] = 0;
+										// Rule 2
+										}else if((neighbours == 2) || (neighbours == 3)){
+											game[activeCell] = 1;
+
+										// Rule 3 - Overpopulation
+										}else if(neighbours > 3){
+											game[activeCell] = 0;
+										}
+										
+										// Kill
+										/* game[i*100+j] = 0; */
+
+								//If dead
+								}else{
+										// Rule 4 - Reproduction
+											if(neighbours == 3){
+												game[activeCell] = 1;
+											}
+											
+											// Revive
+											/* game[i*100+j] = 1; */
+								}
 						}
-				}
 		}
+	}
 }
 
 
@@ -117,6 +162,10 @@ void gameTick(void){
 // 6 7 8
 //
 // A = Active(array index)
+//
+//
+//
+//
 
 int neighbourValid(int position, int active){
 	int y = active/100;
@@ -127,21 +176,21 @@ int neighbourValid(int position, int active){
 		// If corner top left
 		if(y == 0){
 			if((position == 5) || (position == 7) || (position == 8)){
-				return game[position];
+				return getNeighbourStatus(position, active);
 			}else{
 				return 404;
 			}
 		// If corner bottom left
 		}else if(y == 99){
 			if((position == 2) || (position == 3) || (position == 5)){
-				return game[position];
+				return getNeighbourStatus(position, active);
 			}else{
 				return 404;
 			}
 		// Border left no special case
 		}else{
 			if((position == 2) || (position == 3) || (position == 5) || (position == 7) || (position == 8)){
-					return game[position];
+				return getNeighbourStatus(position, active);
 			}else{
 				return 404;
 			}
@@ -151,21 +200,21 @@ int neighbourValid(int position, int active){
 			// If corner top right
 			if(y == 0){
 				if((position == 4) || (position == 6) || (position == 7)){
-					return game[position];
+					return getNeighbourStatus(position, active);
 				}else{
 					return 404;
 				}
 			// If corner bottom right
 			}else if(y == 99){
 				if((position == 1) || (position == 2) || (position == 4)){
-					return game[position];
+					return getNeighbourStatus(position, active);
 				}else{
 					return 404;
 				}
 			// Border right no special case
 			}else{
 				if((position == 1) || (position == 2) || (position == 4) || (position == 6) || (position == 7)){
-					return game[position];
+					return getNeighbourStatus(position, active);
 				}else{
 					return 404;
 				}
@@ -175,21 +224,21 @@ int neighbourValid(int position, int active){
 			// If corner top left
 			if(x == 0){
 				if((position == 5) || (position == 7) || (position == 8)){
-					return game[position];
+					return getNeighbourStatus(position, active);
 				}else{
 					return 404;
 				}
 			// If corner top right
 			}else if(x == 99){
 				if((position == 4) || (position == 6) || (position == 7)){
-					return game[position];
+					return getNeighbourStatus(position, active);
 				}else{
 					return 404;
 				}
 			// Border top no special case
 			}else{
 				if((position == 4) || (position == 5) || (position == 6) || (position == 7) || (position == 8)){
-					return game[position];
+					return getNeighbourStatus(position, active);
 				}else{
 					return 404;
 				}
@@ -199,29 +248,79 @@ int neighbourValid(int position, int active){
 			// If corner bottom left
 			if(x == 0){
 				if((position == 2) || (position == 3) || (position == 5)){
-					return game[position];
+					return getNeighbourStatus(position, active);
 				}else{
 					return 404;
 				}
 			// If corner bottom right
 			}else if(x == 99){
 				if((position == 1) || (position == 2) || (position == 4)){
-					return game[position];
+					return getNeighbourStatus(position, active);
 				}else{
 					return 404;
 				}
 			// Border bottom no special case
 			}else{
 				if((position == 1) || (position == 2) || (position == 3) || (position == 4) || (position == 5)){
-					return game[position];
+					return getNeighbourStatus(position, active);
 				}else{
 					return 404;
 				}
 			}
 	}else{
-		return game[position];
+		return getNeighbourStatus(position, active);
 	}
 	/* printf("x: %d\ny: %d\n\n", x, y); */
 	/* char c; */
 	/* scanf("%c",&c); */
+}
+
+// Returns status of cell <active> in positition <position> 
+// Either 1 or 0 
+//
+// Positions:
+//
+// 1 2 3
+// 4 A 5
+// 6 7 8
+//
+// A = Active(array index)
+int getNeighbourStatus(int position, int active){
+	if(repeat % 2 != 0){
+			if(position == 1){
+				return game[active-100-1];
+			}else if(position == 2){
+				return game[active-100];
+			}else if(position == 3){
+				return game[active-100+1];
+			}else if(position == 4){
+				return game[active-1];
+			}else if(position == 5){
+				return game[active+1];
+			}else if(position == 6){
+				return game[active+100-1];
+			}else if(position == 7){
+				return game[active+100];
+			}else if(position == 8){
+				return game[active+100+1];
+			}
+	}else{
+			if(position == 1){
+				return game2[active-100-1];
+			}else if(position == 2){
+				return game2[active-100];
+			}else if(position == 3){
+				return game2[active-100+1];
+			}else if(position == 4){
+				return game2[active-1];
+			}else if(position == 5){
+				return game2[active+1];
+			}else if(position == 6){
+				return game2[active+100-1];
+			}else if(position == 7){
+				return game2[active+100];
+			}else if(position == 8){
+				return game2[active+100+1];
+			}
+	}
 }
